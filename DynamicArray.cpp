a741f1dc95342824bd53489032160006a9451cd6
Tony11306 <50052865+tony11306@users.noreply.github.com>
2020-09-11 23:53:46 +0800

    Update DynamicArray.cpp

--- DynamicArray.cpp
+++ DynamicArray.cpp
@@ -1,4 +1,4 @@
-# problem source(題目網址): https://www.hackerrank.com/challenges/dynamic-array/problem
+// problem source(題目網址): https://www.hackerrank.com/challenges/dynamic-array/problem
 
 #include <bits/stdc++.h>
 
